4eba29e80071b924c7caf4299884cd3ea03e2330
mwrdrs <158099612+mwrdrs@users.noreply.github.com>
2024-01-30 15:56:51 +0200

    Update style.php

--- style.php
+++ style.php
@@ -2,18 +2,21 @@
 header("Content-type: text/css; charset: UTF-8");
 ?>
 
-@import url('https://fonts.googleapis.com/css2?family=Creepster&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=VCR+OSD+Mono&display=swap');
 
 body {
-    background: url('https://i.pinimg.com/originals/49/f4/16/49f416a4d35244412c69cf7113bc27ba.gif') no-repeat center center fixed; 
+    background: url('https://i.pinimg.com/originals/e7/7f/c3/e77fc3197e445ac3e61e628e0a8cfbf9.gif') no-repeat center center fixed; 
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
     background-size: cover;
-    color: #ff0000;
-    font-family: 'Creepster', cursive;
+    color: #ffffff; 
+    font-family: 'VCR OSD Mono', monospace; 
     text-align: center;
-    padding-bottom: 50px;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
 h1 {
     font-size: 3em;
@@ -26,11 +29,11 @@ p {
 .button {
     display: inline-block;
     border: none;
-    padding: 15px 32px;
+    padding: 10px 20px;
     text-align: center;
     text-decoration: none;
-    font-size: 16px;
-    margin: 4px 2px;
+    font-size: 14px;
+    margin: 40px 2px;
     transition-duration: 0.4s;
     cursor: pointer;
     background-color: #000;
@@ -48,14 +51,14 @@ p {
 }
 
 #developers {
-    color: #ff0000;
+    color: #ffffff; 
     font-family: 'VCR OSD Mono', monospace;
     display: flex;
     justify-content: space-around;
-    margin-top: 20px;
+    margin-top: 50px; 
     background-color: rgba(0, 0, 0, 0.5); 
     border-radius: 15px; 
-    padding: 60px;
+    padding: 30px;
 }
 
 #developers h2 {
@@ -66,7 +69,7 @@ p {
 
 .developer {
     text-align: center;
-    margin: 0 400px; 
+    margin: 0 10px;
 }
 
 .developer img {
@@ -78,6 +81,17 @@ p {
 .developer p {
     font-size: 1.2em;
     text-shadow: 1px 1px #ff0000;
+}
+
+footer {
+    position: relative;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #000;
+    color: white;
+    text-align: center;
+    padding: 0px 3;
 }
 
 @media screen and (max-width: 600px) {
