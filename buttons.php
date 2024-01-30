1cfb5956c6d79a6eff8f1a7ff535ecee26058a12
mwrdrs <158099612+mwrdrs@users.noreply.github.com>
2024-01-30 15:56:02 +0200

    Update buttons.php

--- buttons.php
+++ buttons.php
@@ -1,10 +1,9 @@
-<button class="button" id="buy" onclick="alert('Waiting for Open Beta Testing');">
-    BUY 0€
+<button class="button" id="h" onclick="alert('Waiting for Open Beta Testing');">
+    FREE
 </button>
-<button class="button" id="openModal">Description</button>
 <footer>
     <p>hightlight 2024 ©</p>
-    <button class="minisota" onclick="window.location.href='https://discord.gg/dN55RYYPjT'">
-        <img src="https://i.pinimg.com/564x/f7/41/2e/f7412e6b1936bc97676495f2ae993eb3.jpg" width="30" height="30" alt="Discord Icon">
-    </button>
+    <button class="button" id="asd" onclick="window.location.href='https://github.com/mwrdrs/hightlight_site/tree/main'">
+            source code
+</button>
 </footer>
